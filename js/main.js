e40ef6e2d92d1ca805d527bbd90bdd6794e1361f
ssrapid <luigi12.1007@gmail.com>
2026-01-22 01:42:43 +0900

    loadHtmlの修正

--- js/main.js
+++ js/main.js
@@ -11,11 +11,11 @@ export { state } from "./state.js"
 function main() {
   const promise_tab1 = loadHTML(
     document.getElementById("tab__panel-main"),
-    "../tabpanels/main.html"
+    "tabpanels/main.html"
   ).then (panel => {
     const root = panel.querySelector("#main-table-root");
     // buildMainTable(root);
-    loadHTML(root, "../tabpanels/mainTable.html");
+    loadHTML(root, "tabpanels/mainTable.html");
     return {panel, root};
   }).then (({panel, root}) => {
     tabPanel_main(root);
@@ -24,7 +24,7 @@ function main() {
 
   const promise_tab2 = loadHTML(
     document.getElementById("tab__panel-rule"),
-    "../tabpanels/rule.html"
+    "tabpanels/rule.html"
   ).then (panel => {
     tabPanel_rule(panel);
     return panel;
